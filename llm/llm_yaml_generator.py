877d250a17aa07d318e63895a3756bdbbdb42501
isi3 <isi3@alumni.uv.es>
2026-05-12 23:57:07 +0200

    Format Python files

--- llm/llm_yaml_generator.py
+++ llm/llm_yaml_generator.py
@@ -55,9 +55,7 @@ def validate_llm_yaml(
     )
 
     selector_labels = (
-        deployment.get("spec", {})
-        .get("selector", {})
-        .get("matchLabels", {})
+        deployment.get("spec", {}).get("selector", {}).get("matchLabels", {})
     )
 
     service_selector = service.get("spec", {}).get("selector", {})
